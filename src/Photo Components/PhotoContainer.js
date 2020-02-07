84b27932739523e4b038cce35e6e34eec34c0261
Alex <Magana@Alexs-MacBook-Air.local>
2020-02-06 21:44:03 -0600

    fixed other errors

---
+++ src/Photo Components/PhotoContainer.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import PhotoContent from './PhotoContent';
+
+const PhotoContainer = () => {
+    const [nasa, setNasa] = useState([]);
+
+    const effectCallBackFn = () => {
+        axios
+            .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+            .then(response => setNasa(response.data))
+            .catch(error => console.log(error));
+    }
+    
+    useEffect(effectCallBackFn, []);
+
+    console.log('initial render', nasa);
+    
+    return (
+        <div className="photo-card-container">
+            <PhotoContent data={nasa} /> 
+        </div>
+    );
+};
+
+export default PhotoContainer;
---
+++ src/Photo Components/PhotoContent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const PhotoContent = props => {
+    console.log(props);
+
+    return (
+        <div className="photo-card">
+            <h1>Photo of the day</h1>
+            <img className="photo-image" src={props.data.url} alt="ADOP"/>
+            <h3>{props.data.title}</h3>
+            <p className="center">{props.data.date}</p>
+            <p className="center">By: {props.data.copyright}</p>
+            <p>Description: {props.data.explanation}</p>
+        </div>
+    )
+}
+export default PhotoContent;
